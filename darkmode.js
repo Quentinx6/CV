2db62fb789951166e1734255c026782d9fc811c5
Quentinx6 <quentin.lecompte@3wa.io>
2021-06-25 14:45:16 +0200

    Image de moi

--- darkmode.js
+++ darkmode.js
@@ -2,22 +2,17 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let button = document.getElementById("bouton");
     let body = document.body;
-    if (returnProut()){
-        body.classList.add("darkmode")
-    }
-    else{
-        body.classList.remove("darkmode");
-    }
-    function returnProut(){
-
-        let proute;
-        console.log(proute)
-
         button.addEventListener("click", function() {
-            proute = true;
             body.classList.toggle("darkmode");
-            console.log(proute)
+            if (body.classList.contains("darkmode")){
+                let msg = "Mode clair";
+                button.innerHTML = msg;
+            }
+            else{
+                let msg = "Mode sombre";
+                button.innerHTML = msg;
+            }
         });
-        return proute;
-    }
+
+
 })
